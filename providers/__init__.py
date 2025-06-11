0824af8471956d9361b0c775264145aba324f540
Paweł Nowak <apach87@gmail.com>
2025-06-12 01:17:05 +0200

    feat: add FileContextProvider with JSON persistence

--- providers/__init__.py
+++ providers/__init__.py
@@ -3,6 +3,7 @@
 
 from .memory_context_provider import MemoryContextProvider
 from .file_based_context_provider import FileBasedContextProvider
+from .file_context_provider import FileContextProvider
 from .redis_context_provider import RedisContextProvider
 from .mock_context_provider import MockContextProvider
 from .simple_context_provider import SimpleContextProvider
@@ -10,6 +11,7 @@ from .simple_context_provider import SimpleContextProvider
 __all__ = [
     "MemoryContextProvider",
     "FileBasedContextProvider",
+    "FileContextProvider",
     "RedisContextProvider",
     "MockContextProvider",
     "SimpleContextProvider",
