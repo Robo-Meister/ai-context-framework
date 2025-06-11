c1e45541503241200d38f2e3a0df04f44ee35f43
Paweł Nowak <apach87@gmail.com>
2025-06-12 01:14:39 +0200

    Add SQLite context provider (#27)

--- providers/__init__.py
+++ providers/__init__.py
@@ -6,6 +6,7 @@ from .file_based_context_provider import FileBasedContextProvider
 from .redis_context_provider import RedisContextProvider
 from .mock_context_provider import MockContextProvider
 from .simple_context_provider import SimpleContextProvider
+from .sqlite_context_provider import SQLiteContextProvider
 
 __all__ = [
     "MemoryContextProvider",
@@ -13,4 +14,5 @@ __all__ = [
     "RedisContextProvider",
     "MockContextProvider",
     "SimpleContextProvider",
+    "SQLiteContextProvider",
 ]
