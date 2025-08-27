3430322699989993fb2937b53d7f28bd177936fe
Paweł Nowak <apach87@gmail.com>
2025-08-27 20:54:00 +0200

    test: fix duplicate import

--- src/caiengine/pipelines/configurable_pipeline.py
+++ src/caiengine/pipelines/configurable_pipeline.py
@@ -14,6 +14,7 @@ from caiengine.providers import (
 )
 from caiengine.interfaces.context_provider import ContextProvider
 from caiengine.inference.dummy_engine import DummyAIInferenceEngine
+from caiengine.inference.token_usage_tracker import TokenUsageTracker
 from caiengine.core.trust_module import TrustModule
 from caiengine.core.goal_feedback_loop import GoalDrivenFeedbackLoop
 from caiengine.core.goal_strategies.simple_goal_strategy import SimpleGoalFeedbackStrategy
@@ -73,11 +74,13 @@ class ConfigurablePipeline:
                 output_size=feedback_cfg.get("output_size", 1),
                 parser=parser,
             )
+            engine = TokenUsageTracker(manager.inference_engine)
+            manager.inference_engine = engine
             pipeline = FeedbackPipeline(
-                provider, manager.inference_engine, learning_manager=manager
+                provider, engine, learning_manager=manager
             )
         elif feedback_cfg.get("type") == "goal":
-            engine = DummyAIInferenceEngine()
+            engine = TokenUsageTracker(DummyAIInferenceEngine())
             pipeline = FeedbackPipeline(provider, engine)
             strategy = SimpleGoalFeedbackStrategy(
                 feedback_cfg.get("one_direction_layers", [])
